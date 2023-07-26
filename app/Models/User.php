e3ed1188e2e1e957c51be9f93a5f066021816cbe
thomaswinchestar <thomasanthony999@gmail.com>
2023-07-27 02:18:33 +0630

    modified usr model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Filament\Models\Contracts\FilamentUser;
 
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -46,10 +45,4 @@ class User extends Authenticatable implements FilamentUser
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    private mixed $email;
-
-    public function canAccessFilament(): bool
-    {
-        return $this->email == 'admin@gmail.com' && $this->hasVerifiedEmail();
-    }
 }
